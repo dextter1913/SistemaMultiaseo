d55fa6c300b4e8b31ed8616f9bf11c512c1fc6eb
dextter1913 <dextter1913@gmail.com>
2021-05-06 17:36:50 -0500

    Agregando modulo zonas

---
+++ principal/zonas.php
@@ -0,0 +1,16 @@
+<?php require_once '../estructura/superior.php' ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4"></div>
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 shadow p-3 mb-5 bg-body rounded">
+            <form action="zonas.php" method="post">
+                <label for="nombrezona" title="Nombre de nueva zona">Nombre Zona</label>
+                <input type="text" name="nombrezona" id="nombrezona" class="form-control" placeholder="Nombre de nueva zona">
+                <button type="submit" class="btn btn-outline-secondary btn-lg">Agregar Zona</button>
+            </form>
+        </div>
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4"></div>
+    </div>
+</div>
+
+<?php require_once '../estructura/inferior.php' ?>
