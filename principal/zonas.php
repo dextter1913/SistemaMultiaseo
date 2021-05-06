35261161dd6804d4990a3be78e8b562d414664f5
dextter1913 <dextter1913@gmail.com>
2021-05-06 17:38:07 -0500

    Puliendo modulo zona

--- principal/zonas.php
+++ principal/zonas.php
@@ -5,7 +5,7 @@
         <div class="col-12 col-sm-4 col-md-4 col-lg-4 shadow p-3 mb-5 bg-body rounded">
             <form action="zonas.php" method="post">
                 <label for="nombrezona" title="Nombre de nueva zona">Nombre Zona</label>
-                <input type="text" name="nombrezona" id="nombrezona" class="form-control" placeholder="Nombre de nueva zona">
+                <input type="text" name="nombrezona" id="nombrezona" class="form-control" placeholder="Nombre de nueva zona"> <br/>
                 <button type="submit" class="btn btn-outline-secondary btn-lg">Agregar Zona</button>
             </form>
         </div>
