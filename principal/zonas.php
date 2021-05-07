d8466e113bf7c6f0e33f80da8fce1e3f91805789
dextter1913 <dextter1913@gmail.com>
2021-05-07 13:06:50 -0500

    Agregando tabla para mostrar zonas

--- principal/zonas.php
+++ principal/zonas.php
@@ -1,19 +1,38 @@
-<?php 
+<?php
 require_once '../estructura/superior.php';
 require_once '../clases/AbrirZonas.php';
 ?>
 <div class="container-fluid">
-    <form action="zonas.php" method="post">
-        <div class="row">
-            <div class="col-12 col-sm-4 col-md-4 col-lg-4"></div>
-            <div class="col-12 col-sm-4 col-md-4 col-lg-4 shadow p-3 mb-5 bg-body rounded">
+    <div class="row">
+        <div class="col-12 col-md-1 col-lg-1"></div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4 shadow p-3 mb-5 bg-body rounded">
+            <form action="zonas.php" method="post">
                 <label for="nombrezona" title="Nombre de nueva zona">Nombre Zona</label>
                 <input type="text" name="nombrezona" id="nombrezona" class="form-control" placeholder="Nombre de nueva zona"> <br />
                 <button type="submit" name="btnagregarzona" class="btn btn-outline-secondary btn-lg">Agregar Zona</button>
-            </div>
-            <div class="col-12 col-sm-4 col-md-4 col-lg-4"></div>
+            </form>
         </div>
-    </form>
+        <div class="col-12 col-md-1 col-lg-1"></div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6 table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>
+                            <center>Numero de zona</center>
+                        </th>
+                        <th>
+                            <center>Nombre de la zona</center>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
     <?php
 
     if (isset($_POST['btnagregarzona'])) {
