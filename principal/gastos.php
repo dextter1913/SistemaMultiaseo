3382f1359707f1e23d70596cd4b521424d7fe2b2
dextter1913 <dextter1913@gmail.com>
2021-05-08 14:11:47 -0500

    Agregandole trycatch a la clase insertarclientes y creando formulario para ingresar gasts variables y fijos

--- principal/gastos.php
+++ principal/gastos.php
@@ -1,5 +1,20 @@
-<?php require_once '../estructura/superior.php';?>
+<?php require_once '../estructura/superior.php'; ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4 shadow p-3 mb-5 bg-body rounded">
+            <form action="gastos.php" method="post">
+                <label for="fijos">Gastos Fijos</label>
+                <input type="number" name="fijos" id="fijos" placeholder="Ingrese Gastos fijos" title="Gastos fijos" class="form-control"></br>
+                <label for="variables">Gastos Variables</label>
+                <input type="number" name="variables" id="variables" placeholder="Ingrese Gastos Variables" title="Gastos Variables" class="form-control">
+                </br><center><button class="btn btn-outline-secondary btn-lg"><i class="fas fa-cash-register"></i></button></center>
+            </form>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4">
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 col-lg-4"></div>
+    </div>
+</div>
 
 
-
-<?php require_once '../estructura/inferior.php';?>
+<?php require_once '../estructura/inferior.php'; ?>
