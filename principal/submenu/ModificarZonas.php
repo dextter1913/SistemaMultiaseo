1b2d5c4bdadcafb34842635d39e773f229c39cfd
dextter1913 <dextter1913@gmail.com>
2021-05-09 14:24:17 -0500

    organizando codigo

--- principal/submenu/ModificarZonas.php
+++ principal/submenu/ModificarZonas.php
@@ -11,7 +11,7 @@ require_once '../../clases/ModificarZonas.php';
             <form action="ModificarZonas.php?id=<?php echo $_GET['id']; ?>" method="post"><?php //Recibiendo desde metodo get el id de el formulario zonas.php?>
                 <label for="NombreZona" title="Modificar Nombre de a zona">Ingrese el nuevo nombre de la zona</label>
                 <input type="text" name="NombreZona" id="NombreZona" class="form-control" placeholder="Nombre Zona">
-               <br> <input type="submit" value="Modificar" class="btn btn-outline-primary" name="btnModificarZona">
+                <br> <input type="submit" value="Modificar" class="btn btn-outline-primary" name="btnModificarZona">
             </form>
         </div>
         <div class="col-12 col-sm-4 col-md-4 col-lg-4"></div>
