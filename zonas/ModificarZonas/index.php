791ba217e2e78ab8ae76cf4188f36dda116ea7ed
dextter1913 <dextter1913@gmail.com>
2021-05-15 15:30:34 -0500

    Agregando Ruta correcta en zonas

--- zonas/ModificarZonas/index.php
+++ zonas/ModificarZonas/index.php
@@ -8,7 +8,7 @@ require_once '../../clases/ModificarZonas.php';
     <div class="row">
         <div class="col-12 col-sm-4 col-md-4 col-lg-4"></div>
         <div class="col-12 col-sm-4 col-md-4 col-lg-4 shadow p-3 mb-5 bg-body rounded">
-            <form action="../ModificarZonas/?id=<?php echo $_GET['id']; ?>" method="post"><?php //Recibiendo desde metodo get el id de el formulario zonas.php?>
+            <form action="../../zonas/?id=<?php echo $_GET['id']; ?>" method="post"><?php //Recibiendo desde metodo get el id de el formulario zonas.php?>
                 <label for="NombreZona" title="Modificar Nombre de a zona">Ingrese el nuevo nombre de la zona</label>
                 <input type="text" name="NombreZona" id="NombreZona" class="form-control" placeholder="Nombre Zona">
                 <br> <input type="submit" value="Modificar" class="btn btn-outline-primary" name="btnModificarZona">
