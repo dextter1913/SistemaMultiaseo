70b48f1ec825a8f36e2e5d0a3ba359499d18dcfb
dextter1913 <dextter1913@gmail.com>
2021-05-15 16:52:48 -0500

    Editando nombre a ProcesoModificarZonas.php

--- zonas/ModificarZonas/index.php
+++ zonas/ModificarZonas/index.php
@@ -7,7 +7,7 @@ require_once '../../estructura/menu2.php';
     <div class="row">
         <div class="col-12 col-sm-4 col-md-4 col-lg-4"></div>
         <div class="col-12 col-sm-4 col-md-4 col-lg-4 shadow p-3 mb-5 bg-body rounded">
-            <form action="../../PuertaTrasera/ModificarZonas.php?id=<?php echo $_GET['id']; ?>" method="post"><?php //Recibiendo desde metodo get el id de el formulario zonas.php?>
+            <form action="../../PuertaTrasera/ProcesoModificarZonas.php?id=<?php echo $_GET['id']; ?>" method="post"><?php //Recibiendo desde metodo get el id de el formulario zonas.php?>
                 <label for="NombreZona" title="Modificar Nombre de a zona">Ingrese el nuevo nombre de la zona</label>
                 <input type="text" name="NombreZona" id="NombreZona" class="form-control" placeholder="Nombre Zona">
                 <br> <input type="submit" value="Modificar" class="btn btn-outline-primary" name="btnModificarZona">
