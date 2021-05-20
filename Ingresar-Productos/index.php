3dc9e4f8e0de50d9d54e6cdf04cf766528b94a7f
dextter1913 <dextter1913@gmail.com>
2021-05-20 15:45:56 -0500

    prueba 8

--- Ingresar-Productos/index.php
+++ Ingresar-Productos/index.php
@@ -69,11 +69,14 @@ if (isset($_SESSION['usuario'])) {
                             </div>
                             </form>
                             <?php
-                            if (isset($_POST['btningresarproducto'])) {
-                                require_once '../clases/IngresarProductos.php';
-                                $IngresProductos = new IngresarProductos($_POST['nombre'], $_POST['precio'], $_POST['precioVenta'], $_POST['categoria'], $_POST['DescripcionP'], $_POST['Stock'], $_POST['usuario']); //enviando los datos por metodo post en el parametro del objeto 
-                                $IngresProductos->InsertarProducto();
+                            if (isset($_SESSION['prueba'])) {
+                                echo $_SESSION['prueba'];
                             }
+                            //if (isset($_POST['btningresarproducto'])) {
+                            //    require_once '../clases/IngresarProductos.php';
+                            //    $IngresProductos = new IngresarProductos($_POST['nombre'], $_POST['precio'], $_POST['precioVenta'], $_POST['categoria'], $_POST['DescripcionP'], $_POST['Stock'], $_POST['usuario']); //enviando los datos por metodo post en el parametro del objeto 
+                            //    $IngresProductos->InsertarProducto();
+                            //}
                             //if (isset($_SESSION['Mensaje'])) {
                             //    print $_SESSION['Mensaje'];
                             //    unset($_SESSION['Mensaje']);
--- PuertaTrasera/ProcesoIngresarProducto.php
+++ PuertaTrasera/ProcesoIngresarProducto.php
@@ -1,4 +1,10 @@
 <?php 
-echo $_POST['nombre'].' '.$_POST['precio'].' '.$_POST['precioVenta'].' '.$_POST['categoria'].' '.$_POST['DescripcionP'].' '.$_POST['Stock'].' '.$_POST['usuario'];
+//echo $_POST['nombre'].' '.$_POST['precio'].' '.$_POST['precioVenta'].' '.$_POST['categoria'].' '.$_POST['DescripcionP'].' '.$_POST['Stock'].' '.$_POST['usuario'];
+require_once '../clases/IngresarProductos.php';
+$IngresProductos = new IngresarProductos($_POST['nombre'], $_POST['precio'], $_POST['precioVenta'], $_POST['categoria'], $_POST['DescripcionP'], $_POST['Stock'], $_POST['usuario']); //enviando los datos por metodo post en el parametro del objeto 
+$IngresProductos->InsertarProducto();
+session_start();
+$_SESSION['prueba'] = "<center>Producto ingresado correctamente</center>";
+header('Location:../Ingresar-Productos');
 
 ?>
