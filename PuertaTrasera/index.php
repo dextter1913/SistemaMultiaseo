6a3294e0d8fa390aeeefd277f4aaccb64fea0207
dextter1913 <dextter1913@gmail.com>
2021-05-20 14:28:11 -0500

    prueba 6

--- PuertaTrasera/index.php
+++ PuertaTrasera/index.php
@@ -1,8 +1,11 @@
 
-<?php 
-        require_once '../clases/IngresarProductos.php';
-        $IngresProductos = new IngresarProductos($_GET['nombre'], $_GET['precio'], $_GET['precioVenta'], $_GET['categoria'], $_GET['DescripcionP'], $_GET['Stock'], $_GET['usuario']); //enviando los datos por metodo post en el parametro del objeto 
-        $IngresProductos->InsertarProducto();
-        session_start();
-        $_SESSION['Mensaje'] = "<center>Producto Ingresado Correctamente</center>";
-        header('Location:../registro/'); ?>
+<?php
+if (isset($_GET['btningresarproducto'])) {
+    require_once '../clases/IngresarProductos.php';
+    $IngresProductos = new IngresarProductos($_GET['nombre'], $_GET['precio'], $_GET['precioVenta'], $_GET['categoria'], $_GET['DescripcionP'], $_GET['Stock'], $_GET['usuario']); //enviando los datos por metodo post en el parametro del objeto 
+    $IngresProductos->InsertarProducto();
+    session_start();
+    $_SESSION['Mensaje'] = "<center>Producto Ingresado Correctamente</center>";
+    header('Location:../registro/');
+}
+?>
