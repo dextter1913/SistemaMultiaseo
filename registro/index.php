61f741a5637e822c929476404ba8c35dfd2f9bee
dextter1913 <dextter1913@gmail.com>
2021-05-20 12:24:32 -0500

    requiriendo la clase IngresarProductos

--- registro/index.php
+++ registro/index.php
@@ -15,6 +15,7 @@ if (isset($_SESSION['usuario'])) {
 ?>
 <?php
 if (isset($_POST['btnregistrar'])) {
+    require_once '../clases/IngresarProductos.php';
     $IngresProductos = new IngresarProductos($_POST['nombre'], $_POST['precio'], $_POST['precioVenta'], $_POST['categoria'], $_POST['DescripcionP'], $_POST['Stock'], $_POST['usuario']); //enviando los datos por metodo post en el parametro del objeto 
     $IngresProductos->InsertarProducto();
 }
