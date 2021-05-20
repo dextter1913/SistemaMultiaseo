43184069907da3a57de8ebc986d4694e2b513c38
dextter1913 <dextter1913@gmail.com>
2021-05-20 14:03:35 -0500

    prueba2

--- PuertaTrasera/ProcesoIngresarProd.php
+++ PuertaTrasera/ProcesoIngresarProd.php
@@ -8,12 +8,12 @@
 </head>
 <body>
         <h1>hola</h1>
-</body>
-</html>
-<?php 
+        <?php 
         require_once '../clases/IngresarProductos.php';
         $IngresProductos = new IngresarProductos($_GET['nombre'], $_GET['precio'], $_GET['precioVenta'], $_GET['categoria'], $_GET['DescripcionP'], $_GET['Stock'], $_GET['usuario']); //enviando los datos por metodo post en el parametro del objeto 
         $IngresProductos->InsertarProducto();
         session_start();
         $_SESSION['Mensaje'] = "<center>Producto Ingresado Correctamente</center>";
-        header('Location:../registro/');
+        header('Location:../registro/'); ?>
+</body>
+</html>
